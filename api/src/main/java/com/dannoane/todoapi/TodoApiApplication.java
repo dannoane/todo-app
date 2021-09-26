3eff340b0603daf18c865ff71f6b2ddfd8e6b466
Dan Noane <noane.dan@outlook.com>
2021-09-26 12:15:54 +0300

    add cors

--- api/src/main/java/com/dannoane/todoapi/TodoApiApplication.java
+++ api/src/main/java/com/dannoane/todoapi/TodoApiApplication.java
@@ -2,10 +2,23 @@ package com.dannoane.todoapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class TodoApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TodoApiApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("http://localhost:3000");
+			}
+		};
 	}
 }

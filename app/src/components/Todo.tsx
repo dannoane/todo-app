6dbbbefeb542387996a3eb896d35b41755381ac3
Dan Noane <noane.dan@outlook.com>
2021-09-27 19:22:17 +0300

    ui updates

--- app/src/components/Todo.tsx
+++ app/src/components/Todo.tsx
@@ -1,6 +1,6 @@
 import React, { useState, FunctionComponent } from 'react';
-import { Paper, Checkbox, Typography, IconButton } from '@mui/material';
-import DeleteIcon from '@mui/icons-material/Delete';
+import { Paper, Card, Checkbox, Typography, IconButton } from '@mui/material';
+import ClearIcon from '@mui/icons-material/Clear';
 import { StoreObject, useMutation } from '@apollo/client';
 import { Todo as TodoType, SET_TODO_STATUS, DELETE_TODO } from '../graph/Todo';
 
@@ -9,14 +9,14 @@ interface TodoProps {
 }
 
 const Todo: FunctionComponent<TodoProps> = ({ todo }) => {
-    const [completed, setCompleted] = useState(todo.completed);
+    const [isDeleteHidden, setDeleteHidden] = useState(true);
+    
     const [setTodoStatus, { loading: todoStatusLoading, error: todoStatusError }] = useMutation(SET_TODO_STATUS);
     const [deleteTodo, { loading: deleteTodoLoading, error: deleteTodoError }] = useMutation(DELETE_TODO, {
         update(cache, { data: { deleteTodo } }) {
             cache.modify({
                 fields: {
                     allTodos(existingTodos = [], { readField }) {
-                        console.log(existingTodos, todo.id);
                         return existingTodos.filter((todoRef: StoreObject) => todo.id !== readField('id', todoRef));
                     }
                 }
@@ -26,26 +26,27 @@ const Todo: FunctionComponent<TodoProps> = ({ todo }) => {
         }
     });
 
-    const handleChangeStatus = (completed: boolean) => {
-        setCompleted(completed);
-        setTodoStatus({ variables: { id: todo.id, completed } });
-    }
-
     return (
         <Paper elevation={3} sx={{
             margin: '5px',
-            display: 'flex',
-            flexDirection: 'row',
-            alignItems: 'center',
         }}>
-            <Checkbox checked={completed} onChange={() => handleChangeStatus(!completed)} />
-            <Typography align="left" variant="body1">{todo.text}</Typography>
-            <IconButton aria-label="delete todo" onClick={() => deleteTodo({ variables: { id: todo.id } })} sx={{
-                marginLeft: 'auto',
-                marginRight: 0,
+            <Card onMouseOver={() => setDeleteHidden(false)} onMouseLeave={() => setDeleteHidden(true)} sx={{
+                display: 'flex',
+                flexDirection: 'row',
+                alignItems: 'center',
             }}>
-                <DeleteIcon />
-            </IconButton>
+                <Checkbox checked={todo.completed} onChange={() => setTodoStatus({ variables: { id: todo.id, completed: !todo.completed } }) } />
+                <Typography align="left" variant="body1">{todo.text}</Typography>
+                {isDeleteHidden
+                    ? (<></>)
+                    : (<IconButton aria-label="delete todo" hidden={isDeleteHidden} onClick={() => deleteTodo({ variables: { id: todo.id } })} sx={{
+                            marginLeft: 'auto',
+                            marginRight: 0,
+                        }}>
+                            <ClearIcon />
+                        </IconButton>)
+                }
+            </Card>
         </Paper >
     );
 }

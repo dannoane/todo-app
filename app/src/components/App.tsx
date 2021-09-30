8015807621df4e7fa305d1c899b022225e443153
Dan Noane <noane.dan@outlook.com>
2021-09-30 19:44:03 +0300

    styling

--- app/src/components/App.tsx
+++ app/src/components/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container, Paper, Divider } from '@mui/material';
+import { Card, Divider } from '@mui/material';
 import { useQuery } from '@apollo/client';
 import { Todo, FETCH_ALL_TODOS } from '../graph/Todo';
 import { Todo as TodoComponent } from './Todo';
@@ -9,23 +9,22 @@ export default function App() {
     const { loading, error, data: { allTodos = [] } = {} } = useQuery(FETCH_ALL_TODOS);
 
     return (
-        <Container maxWidth='md' sx={{ background: 'blue' }}>
-            <Paper elevation={3} sx={{
-                margin: 0,
-                padding: '20px',
-                position: 'absolute',
-                top: '50%',
-                left: '50%',
-                transform: 'translate(-50%, -50%)',
-                minWidth: '500px',
-                backgroundColor: '#ffea00',
-            }}>
-                {allTodos.map((todo: Todo) => (
-                    <TodoComponent key={todo.id.toString()} todo={todo} />
-                ))}
-                <Divider sx={{ marginTop: '15px', marginBottom: '15px' }} />
-                <NewTodo />
-            </Paper>
-        </Container>
+        <Card elevation={3} sx={{
+            margin: 0,
+            padding: '20px',
+            position: 'absolute',
+            top: '50%',
+            left: '50%',
+            transform: 'translate(-50%, -50%)',
+            minWidth: '500px',
+            backgroundColor: '#ffea00',
+            borderRadius: '20px',
+        }}>
+            {allTodos.map((todo: Todo) => (
+                <TodoComponent key={todo.id.toString()} todo={todo} />
+            ))}
+            <Divider sx={{ marginTop: '15px', marginBottom: '15px', borderWidth: '1px', borderColor: '#000000', borderRadius: '1px', backgroundColor: '#000000' }} />
+            <NewTodo />
+        </Card>
     );
 }
